0057e6cb51ffc912284ec75a209bd416cdf4f106
KrisLu116420 <kl116420@126.com>
2023-03-21 13:44:02 +0800

    study

--- clrnet/datasets/registry.py
+++ clrnet/datasets/registry.py
@@ -38,17 +38,17 @@ def build_dataloader(split_cfg, cfg, is_train=True):
 
     dataset = build_dataset(split_cfg, cfg)
 
-    init_fn = partial(worker_init_fn, seed=cfg.seed)
+    init_fn = partial(worker_init_fn, seed=cfg.seed)  # 根据cfg.seed定义的random seed的函数，每个worker_id有一个相应的seed
 
     samples_per_gpu = cfg.batch_size // cfg.gpus
     data_loader = torch.utils.data.DataLoader(
         dataset,
         batch_size=cfg.batch_size,
-        shuffle=shuffle,
-        num_workers=cfg.workers,
-        pin_memory=False,
-        drop_last=False,
-        collate_fn=partial(collate, samples_per_gpu=samples_per_gpu),
-        worker_init_fn=init_fn)
+        shuffle=shuffle,   # 为True时会在每个epoch重新打乱数据
+        num_workers=cfg.workers,   # 用多少个子进程加载数据。0表示数据将在主进程中加载
+        pin_memory=False,   # 设置pin_memory=True，则意味着生成的Tensor数据最开始是属于内存中的锁页内存，这样将内存的Tensor转义到GPU的显存就会更快一些。
+        drop_last=False,   # 如果数据集大小不能被batch size整除，则设置为True后可删除最后一个不完整的batch。如果设为False并且数据集的大小不能被batch size整除，则最后一个batch将更小。
+        collate_fn=partial(collate, samples_per_gpu=samples_per_gpu),   # 将一个batch的数据和标签进行合并操作。
+        worker_init_fn=init_fn)   #  如果不是None，在seeding之后，数据加载之前，每个worker子进程将会调用这个函数，并把worker_id作为输入。
 
     return data_loader
